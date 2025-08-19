67b089e44e107dea68a5e4d26475960f474fa71f
gzulczyk <ginttermail@gmail.com>
2025-08-19 22:27:03 +0200

    make first line codes of armv7 asm create declarative section of .section, .global and definied _start func, main func and beginning of display_prompt func

--- display.s
+++ display.s
@@ -1,1 +1,16 @@
-// I'll provide here a solution which is gonna display the prompt
+.section text @ we want execute that code, so we have to use the text section, not data, etc
+.global _start @ declaration of program start point
+
+prompt: .asciz "X " @ declaration of ascii sign of our shell, i decied to make anothar than Ed from LLTV and i use X instead of dolar sign :) 
+
+_start:
+    bl main @ switch to the main function, _start is the start point of whole program
+
+main:
+    bl display_prompt @ switch to display_prompt function
+    b main            @ switch again to the begining of main
+
+display_prompt: 
+    push { lr }
+    ldr r0, =prompt 
+    bl print_string
