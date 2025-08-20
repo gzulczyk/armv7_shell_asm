12788950a8beff79be9cbf8ab5eee8ad09b32810
gzulczyk <ginttermail@gmail.com>
2025-08-20 21:34:57 +0200

    Created display instruction using armv7 calls

--- display.s
+++ display.s
@@ -1,4 +1,4 @@
-.section text @ we want execute that code, so we have to use the text section, not data, etc
+.section .text @ we want execute that code, so we have to use the text section, not data, etc
 .global _start @ declaration of program start point
 
 prompt: .asciz "X " @ declaration of ascii sign of our shell, i decied to make anothar than Ed from LLTV and i use X instead of dolar sign :) 
@@ -17,3 +17,21 @@ display_prompt:
     pop {pc} @ go back to the main func via using the lr saved in the beginning in that declared func
 
 print_string:
+    push {lr}
+    mov r1, r0 @ pointer to actual letter
+    mov r2, #0 @ index of char
+
+_print_loop: 
+    ldrb r3, [r1, r2] @ r1, actual letter, r2 actual index
+    cmp r3, #0
+    beq _end_print
+    add r2, r2, #1
+    b _print_loop
+
+_end_print:
+    mov r7, #4 @ call to sys_write, more at https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md#arm-32_bit_EABI
+    mov r0, #1 @ because stdout = 1, more at https://man7.org/linux/man-pages/man3/stdin.3.html
+    svc #0 @ execution of system call
+    pop {pc} @ return to display_prompt
+
+@ this whole process shows how to print "X ", if you go from the top to the bottom, that instructions gonna literally output "X " once, but if someone didnt stop, this thing gonna be execudet indefinitely
