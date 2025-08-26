ee9576e34c5dbadd5e14cda885ed049172c84f46
gzulczyk <ginttermail@gmail.com>
2025-08-26 07:04:22 +0200

    Made read.s as separate function file

--- read.s
+++ read.s
@@ -1,1 +1,14 @@
-// I'll provide here a solution where I'm gonna read input provided to my shell
+.section .data
+buffer: .space 256
+
+.section .text
+.global read_input
+
+read_input:
+    push {r7, lr}
+    ldr r1, =buffer
+    mov r2, #256
+    mov r7, #3
+    mov r0, #0
+    svc #0
+    pop {r7, pc}
