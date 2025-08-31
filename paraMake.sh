6d57d3a032cfc89d3c7e13603aac97da20acaff3
gzulczyk <ginttermail@gmail.com>
2025-08-31 18:37:32 +0200

    Updated .sh file to make compilation easier

--- paraMake.sh
+++ paraMake.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-
 as -o main.o main.s
-as -o display.o display.s
 as -o read.o read.s
-ld -o shell main.o display.o read.o
+as -o display.o display.s
+as -o execute.o execute.s
+as -o concat.o concat.s
+as -o end.o end.s
+as -o not_found.o not_found.s
+
+ld -o shell main.o read.o display.o execute.o concat.o end.o not_found.o
