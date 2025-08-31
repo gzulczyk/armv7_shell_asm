83587bb4638e46e58eda980d6b4606a6702fd369
gzulczyk <grzegorz.zulczyk@szkolawchmurze.org>
2025-08-31 18:25:11 +0200

    Modified read to support arguments

--- read.s
+++ read.s
@@ -1,14 +1,15 @@
-.section .data
-buffer: .space 256
-
 .section .text
 .global read_input
+.extern buffer 
 
 read_input:
     push {r7, lr}
     ldr r1, =buffer
-    mov r2, #256
+    mov r2, #255
     mov r7, #3
     mov r0, #0
     svc #0
+    add r1, r1, r0
+    mov r2,#0
+    strb r2, [r1]
     pop {r7, pc}
