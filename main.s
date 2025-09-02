b1a1fd4693d03f7e13ca9639a747e9767b5a06b7
gzulczyk <ginttermail@gmail.com>
2025-09-02 06:40:25 +0200

    Updated main.s with unecessary externs to keep program functions connected together

--- main.s
+++ main.s
@@ -2,11 +2,20 @@
 .global _start 
 .extern display_prompt
 .extern read_input
+.extern fork_process 
+.extern strip_newline
+.extern check_terminate
+.extern buffer
+.extern not_found
 
 _start:
     bl main
-    
+
 main: 
     bl display_prompt
     bl read_input
+    ldr r0, =buffer
+    bl strip_newline
+    bl check_terminate
+    bl fork_process
     b main
