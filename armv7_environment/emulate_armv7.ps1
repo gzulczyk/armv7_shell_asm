166bdb764995a328e2ef98ea8a6f72e43220ab51
gzulczyk <ginttermail@gmail.com>
2025-10-02 20:28:14 +0200

    Port of .sh script to the .ps1 one - basic init of file

---
+++ armv7_environment/emulate_armv7.ps1
@@ -0,0 +1,11 @@
+# let's make roll here!
+
+$ISO_MAIN = "https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/armv7/alpine-standard-3.22.1-armv7.iso"
+$ISO_MIRROR = "https://web.archive.org/web/20250823194237/https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/armv7/alpine-standard-3.22.1-armv7.iso"
+
+$ISO_FILE = "alpine.iso"
+$ISO_MD5 = "0cba2a7ba4ce8c1c84fd626071ae6fe4"
+$DISK_IMAGE = "alpine.qcow2"
+$DISK_SIZE = "2G"
+
+Write-Host "Checking required tools..."
